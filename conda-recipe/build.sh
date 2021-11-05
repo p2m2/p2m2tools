ed636b7e1e3c730b06800f68edeef30121b95497
Olivier Filangi <olivier.filangi@gmail.com>
2021-11-05 13:16:20 +0100

    remove conda recipe

--- conda-recipe/build.sh
+++
@@ -1,6 +0,0 @@
-cd $SRC_DIR/
-mkdir -p $PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM
-cp *.jar $PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM
-mkdir -p $PREFIX/bin
-cp $RECIPE_DIR/p2m2tools ${PREFIX}/bin
-chmod 0755 ${PREFIX}/bin/p2m2tools
