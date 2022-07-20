e20adeddbcf53f91168184edd837ef79a5469daf
Olivier Filangi <you@example.com>
2022-07-20 10:45:43 +0200

    implement test stream xlsP2M2

--- src/main/scala/fr/inrae/metabolomics/p2m2/parser/ParserUtils.scala
+++ src/main/scala/fr/inrae/metabolomics/p2m2/parser/ParserUtils.scala
@@ -2,15 +2,25 @@ package fr.inrae.metabolomics.p2m2.parser
 
 object ParserUtils {
 
+  val replace_token : Seq[(String,String)] = Seq(
+    ("$percent","%"),
+    ("$u002E","."),
+    ("$u0020"," "),
+    ("$div","/"),
+    ("$hash","#")
+  )
+
   def getHeaderField[T<: Enumeration](headerField : T, token : String) : Option[T#Value] =
     headerField
       .values
-      .find( _
-        .toString
-        .replace("$percent","%")
-        .replace("$u002E",".")
-        .replace("$u0020"," ")
-        .replace("$div","/")
-        .replace("$hash","#")
-        .equalsIgnoreCase(token))
+      .find( elt => replace_token.foldLeft(elt.toString) {
+        case (accumulator:String,rep : (String,String)) =>
+          accumulator.replace(rep._1,rep._2)
+      }.equalsIgnoreCase(token))
+
+  def toString[T<: Enumeration](v: T#Value) : String =
+    replace_token.foldLeft(v.toString) {
+      case (accumulator:String,rep : (String,String)) =>
+        accumulator.replace(rep._1,rep._2)
+    }
 }
