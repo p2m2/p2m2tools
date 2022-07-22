76885913c22517c0415946374a4927551af315d5
Olivier Filangi <olivier.filangi@gmail.com>
2022-07-22 14:19:31 +0200

    fix codacy recommandations

--- src/main/scala/fr/inrae/metabolomics/p2m2/parser/ParserManager.scala
+++ src/main/scala/fr/inrae/metabolomics/p2m2/parser/ParserManager.scala
@@ -3,16 +3,15 @@ package fr.inrae.metabolomics.p2m2.parser
 import fr.inrae.metabolomics.p2m2.format.MassSpectrometryResultSet
 
 case object ParserManager {
-  def buildMassSpectrometryObject(path : String) : Option[MassSpectrometryResultSet] = {
-    if (GCMSParser.sniffFile(path)) {
+  def buildMassSpectrometryObject(path : String) : Option[MassSpectrometryResultSet] = path match {
+    case _ if GCMSParser.sniffFile(path) =>
       Some(GCMSParser.parse(path))
-    } else if (OpenLabCDSParser.sniffFile(path)) {
+    case _ if OpenLabCDSParser.sniffFile(path) =>
       Some(OpenLabCDSParser.parse(path))
-    } else if (QuantifyCompoundSummaryReportMassLynxParser.sniffFile(path)) {
+    case _ if QuantifyCompoundSummaryReportMassLynxParser.sniffFile(path) =>
       Some(QuantifyCompoundSummaryReportMassLynxParser.parse(path))
-    } else if (XcaliburXlsParser.sniffFile(path)) {
+    case _ if XcaliburXlsParser.sniffFile(path) =>
       Some(XcaliburXlsParser.parse(path))
-    } else
-        None
+    case _ => None
   }
 }
--- src/test/scala/fr/inrae/metabolomics/p2m2/parser/ParserManagerTest.scala
+++ src/test/scala/fr/inrae/metabolomics/p2m2/parser/ParserManagerTest.scala
@@ -8,18 +8,32 @@ object ParserManagerTest extends TestSuite {
   val tests: Tests = Tests {
     test("file empty") {
       assert(ParserManager.buildMassSpectrometryObject("").isEmpty)
-      assert(ParserManager.buildMassSpectrometryObject(getClass.getResource("/GCMS/13CPROT4.txt").getPath).isDefined)
-      assert(ParserManager.buildMassSpectrometryObject(getClass.getResource("/GCMS/13CPROT4.txt").getPath).get.isInstanceOf[GCMS])
+      assert(ParserManager.buildMassSpectrometryObject(
+        getClass.getResource("/GCMS/13CPROT4.txt").getPath).isDefined)
+      ParserManager.buildMassSpectrometryObject(getClass.getResource("/GCMS/13CPROT4.txt").getPath).foreach {
+        x => assert(x.isInstanceOf[GCMS])
+      }
 
-      assert(ParserManager.buildMassSpectrometryObject(getClass.getResource("/OpenLabCDS/Report_Ex1.txt").getPath).isDefined)
-      assert(ParserManager.buildMassSpectrometryObject(getClass.getResource("/OpenLabCDS/Report_Ex1.txt").getPath).get.isInstanceOf[OpenLabCDS])
+      assert(ParserManager.buildMassSpectrometryObject(
+        getClass.getResource("/OpenLabCDS/Report_Ex1.txt").getPath).isDefined)
+      ParserManager.buildMassSpectrometryObject(
+        getClass.getResource("/OpenLabCDS/Report_Ex1.txt").getPath).foreach {
+        x => assert(x.isInstanceOf[OpenLabCDS])
+      }
 
-      assert(ParserManager.buildMassSpectrometryObject(getClass.getResource("/MassLynx/mass_15Ngly.txt").getPath).isDefined)
-      assert(ParserManager.buildMassSpectrometryObject(getClass.getResource("/MassLynx/mass_15Ngly.txt").getPath).get.isInstanceOf[QuantifyCompoundSummaryReportMassLynx])
+      assert(ParserManager.buildMassSpectrometryObject(
+        getClass.getResource("/MassLynx/mass_15Ngly.txt").getPath).isDefined)
+      ParserManager.buildMassSpectrometryObject(
+        getClass.getResource("/MassLynx/mass_15Ngly.txt").getPath).foreach {
+        x => assert(x.isInstanceOf[QuantifyCompoundSummaryReportMassLynx])
+      }
 
-      assert(ParserManager.buildMassSpectrometryObject(getClass.getResource("/Xcalibur/resuts_inj1_Long.XLS").getPath).isDefined)
-      assert(ParserManager.buildMassSpectrometryObject(getClass.getResource("/Xcalibur/resuts_inj1_Long.XLS").getPath).get.isInstanceOf[Xcalibur])
-
+      assert(ParserManager.buildMassSpectrometryObject(
+        getClass.getResource("/Xcalibur/resuts_inj1_Long.XLS").getPath).isDefined)
+      ParserManager.buildMassSpectrometryObject(
+        getClass.getResource("/Xcalibur/resuts_inj1_Long.XLS").getPath).foreach {
+        x => assert(x.isInstanceOf[Xcalibur])
+      }
     }
   }
 }
